7b6c7389194204c25815ec24ee22bda7d2dd0ee9
axnj2 <52408308+axnj2@users.noreply.github.com>
2024-11-21 16:36:56 +0100

    small changes

--- step4/fsk_decode_1_period.m
+++ step4/fsk_decode_1_period.m
@@ -9,7 +9,7 @@ function [decoded_message] = fsk_decode_1_period(signal, f0, delta_f, M, T, Fs)
     end
     
     %verify size of entered signal
-    if not(min(size(signal) == [1, floor(T/Fs)]))
+    if not(all(size(signal) == [1, floor(T/Fs)]))
         dimensions = size(signal);
         error("fsk_decode_1_period: incorrect signal size, should be [%d, %d] and is [%d, %d]", ...
             1, floor(T/Fs), dimensions(1), dimensions(2));    
@@ -18,12 +18,12 @@ function [decoded_message] = fsk_decode_1_period(signal, f0, delta_f, M, T, Fs)
     % projection of the signal on the different base functions
     projections = zeros(1, M);
     for i = 0:M-1
-        [t, base_signal] = fsk_gen_1_period(f0, delta_f, M, T, Fs, i);
+        [~, base_signal] = fsk_gen_1_period(f0, delta_f, M, T, Fs, i);
         projections(i+1) = sum(signal.*base_signal);
     end
 
     % find the maximum projection
-    [max_value, max_index] = max(projections);
+    [~, max_index] = max(projections);
 
     decoded_message = max_index - 1;
 end
