a0df307c79753a8bc85070e08dad36f21d8e6286
axnj2 <52408308+axnj2@users.noreply.github.com>
2024-12-15 17:53:57 +0100

    added support for relative delay duration

--- step4/step4_sender.m
+++ step4/step4_sender.m
@@ -4,7 +4,7 @@ clc; clear; close all hidden;
 % switch between playing sound and saving to file for testing
 play_sound = false;
 
-message_type = "image"; % "text" or "image"
+message_type = "text"; % "text" or "image"
 
 if message_type == "text"
     message = 'hello sound communication';
@@ -28,6 +28,8 @@ T = 8/(2*delta_f);
 
 Delay_before_start = 1000 + 11; % [samples]
 
+% delay between each chunk as a fraction of T
+relative_delay_duration = 0;
 
 
 
@@ -45,24 +47,28 @@ number_of_chunks = length(message_decimal)*2;
 final_signal = [preamble, zeros(1, round(T*Fs))];
 
 for i = 1:number_of_chunks/2
-    byte_signal = encode_byte(message_decimal(i), f0, delta_f, M, T, Fs);
+    byte_signal = encode_byte(message_decimal(i), f0, delta_f, M, T, Fs, relative_delay_duration);
     final_signal = [final_signal, byte_signal];
 end
 
 %normalize the signal
 final_signal = final_signal/max(abs(final_signal));
-final_signal = [zeros(1, Delay_before_start), final_signal];
+
 
 if play_sound
     player = audioplayer(final_signal, Fs, 24, 1); % 1 is the ID of the macbook speaker
     play(player);   
 else
+    % add a delay before the start of the message to test 
+    % the dectection of the start of the message in the receiver
+    final_signal = [zeros(1, Delay_before_start), final_signal];
     audiowrite('step_4_output.wav', final_signal, Fs);
-    save("parameters.mat", "f0", "delta_f", "M", "T", "Fs", "number_of_chunks", "message_type");
+    save("parameters.mat", "f0", "delta_f", "M", "T", "Fs", "number_of_chunks", "message_type",...
+                "relative_delay_duration");
 end
 
 % spectral power density
-
 [spectral_power_density, w] = pwelch(final_signal);
+
 figure;
- plot(w/pi, 10*log10(spectral_power_density));
+ plot(w/pi*Fs/2, 10*log10(spectral_power_density));
