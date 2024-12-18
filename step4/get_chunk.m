a054b3627f4cfb74386cf24796e1ae2d00ff5d25
axnj2 <52408308+axnj2@users.noreply.github.com>
2024-12-18 21:18:19 +0100

    fixed a potential bug where floor would round down a value almost exactly integer

--- step4/get_chunk.m
+++ step4/get_chunk.m
@@ -12,10 +12,10 @@ function [chunk_signal] = get_chunk(recorded_message, chunk_index, start_of_mess
         relative_delay_duration double = 1
     end
 
-    length_of_chunk = floor(chunck_duration*Fs);
+    length_of_chunk = round(chunck_duration*Fs);
 
-    chunk_signal = recorded_message((start_of_message + 1 +  (chunk_index-1)*(1+relative_delay_duration)*length_of_chunk)    + incertitude_window_size/2 :...
-                                    (start_of_message     + ((chunk_index-1)*(1+relative_delay_duration)+1)*length_of_chunk) - incertitude_window_size/2);
+    chunk_signal = recorded_message((start_of_message + 1 +  (chunk_index-1)*(1+relative_delay_duration)    * length_of_chunk)    + incertitude_window_size/2 :...
+                                    (start_of_message     + ((chunk_index-1)*(1+relative_delay_duration)+1) * length_of_chunk) - incertitude_window_size/2);
     %takes into account the incertitude on the intial time
 
 end
